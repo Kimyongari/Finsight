527256959c2677ec00a91c2c48cb458a03403f1c
Yeyeong99 <gongsam99@gmail.com>
2025-09-07 21:00:26 +0900

    [design] 반응형 디자인 수정

--- frontends/corp-advisor--app/src/Screens/HomeScreen.tsx
+++ frontends/corp-advisor--app/src/Screens/HomeScreen.tsx
@@ -30,7 +30,12 @@ function Home() {
     </div>
     <div className="grid grid-cols-2 gap-3">
       <a href="/report" className="group my-3 px-4 py-3 bg-gray-100 rounded-lg hover:bg-gray-700 transition-transform transition-colors duration-200 transform hover:scale-105">
-        <h2 className={h2Class}>기업 분석 보고서 생성</h2>
+        <h2 className={`font-semibold mb-2 group-hover:text-gray-100 transition-colors duration-200
+            font-semibold mb-2 group-hover:text-gray-100 transition-colors duration-200 ${isMobile
+            ? "text-base"
+            : isTablet
+              ? "text-md"
+              : "text-lg"}`}>기업 분석 보고서 생성</h2>
         <h5 className="text-md text-gray-700 group-hover:text-gray-100 transition-colors duration-200">
           기업 소개, 재무 분석, 성장 가능성을 담은
           <br/>
@@ -38,12 +43,12 @@ function Home() {
         </h5>
       </a>
       <a href="/chatbot" className="group my-3 px-4 py-3 bg-gray-100 rounded-lg hover:bg-gray-700 transition-transform transition-colors duration-200 transform hover:scale-105">
-        <h2 className={isMobile
-            ? "text-base font-semibold mb-2 group-hover:text-gray-100 transition-colors duration-200"
+        <h2 className={`font-semibold mb-2 group-hover:text-gray-100 transition-colors duration-200
+            font-semibold mb-2 group-hover:text-gray-100 transition-colors duration-200 ${isMobile
+            ? "text-base"
             : isTablet
-              ? "text-md font-semibold mb-2 group-hover:text-gray-100 transition-colors duration-200"
-              : ""
-}>
+              ? "text-md"
+              : "text-lg"}`}>
           금융 자문 챗봇
         </h2>
         <h5 className="text-md text-gray-700 group-hover:text-gray-100 transition-colors duration-200">
